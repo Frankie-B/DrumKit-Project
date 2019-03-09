7ff646e8d856784b81929e4df97cae61f5a075c4
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-03-09 06:41:04 +0300

    Function - added button click

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector('button').addEventListener('click', handleClick); //Event listner for button click
+
+function handleClick() {
+  //function for button click
+  alert('I got clicked');
+}
